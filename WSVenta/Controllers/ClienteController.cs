e2aed90c89180bf192856c0634d9ff51267386d0
Renato Ticse Lazo <83681731+DYNAMO266@users.noreply.github.com>
2022-04-10 18:49:13 -0500

    Agregar proyecto

---
+++ WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVenta.Models;
+using WSVenta.Models.Response;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try { 
+                    
+                    using (VentaRealContext db =  new VentaRealContext())
+                    {
+                            //Define la variable con solo igualarlo con un numero o un texto
+                            //var lst = 1;
+                            var lst = db.Cliente.ToList();
+
+                        //Indica si el proceso es exitoso y registra en la clase ReSPONSE/rESPUESTA
+                        oRespuesta.Exito = 1;
+                        oRespuesta.Data = lst;  
+                    }
+            }
+            catch (Exception eX)
+            {
+                oRespuesta.Mensaje = eX.Message;
+            }
+            return Ok(oRespuesta);
+
+        }
+
+    }
+}
